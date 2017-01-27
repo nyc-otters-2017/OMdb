fb242f34e076a4223107126433ea98d7484fb556
Nick Himmel <email": null>
2017-01-27 11:21:35 -0500

    Add route

--- app/assets/javascripts/components/Movie.es6.jsx
+++ app/assets/javascripts/components/Movie.es6.jsx
@@ -1,11 +1,13 @@
 class Movie extends React.Component {
   render() {
     let { Title, Year, Poster} = this.props.data
+    let routeToMovie= "/static/"+ this.props.data.imdbID
+    // debugger
     return (
       <article className="movie">
         <li>
           <img src={ Poster } alt="" />
-          <h2>{Title} ({Year})</h2>
+          <a href={routeToMovie}><h2>{Title} ({Year})</h2></a>
         </li>
       </article>
     )
